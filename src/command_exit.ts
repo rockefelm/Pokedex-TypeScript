f9906f009f4b8425f9654313b5023cf0b3826ea9
rockefelm <rockefelm@gmail.com>
2025-08-09 17:06:31 -0400

    added caching functionality for the metadata

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -3,7 +3,10 @@ import { type State } from "./state.js";
  // This function is called when the user wants to exit the application
  export async function commandExit(state: State){
     console.log("Closing the Pokedex... Goodbye!");
+    // Close the readline interface and the PokeAPI cache
     state.readline.close();
+    // Close the PokeAPI cache to ensure all resources are released
+    state.pokeAPI.closeCache();
     // Exit the process with a success code
     process.exit(0);
     // Note: The process.exit(0) is used to terminate the Node.js process
--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,19 +1,46 @@
+import { S } from "vitest/dist/chunks/config.d.D2ROskhv.js";
+import { Cache } from "./pokecache.js";
+
 export class PokeAPI {
-  private static readonly baseURL = "https://pokeapi.co/api/v2";
+    private static readonly baseURL = "https://pokeapi.co/api/v2";
+    #cache: Cache;
 
-  constructor() {}
+    constructor(cacheInterval: number = 300000) { // default 5 minutes
+        this.#cache = new Cache(cacheInterval);
+    }
 
-  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
-    // If no pageURL is provided, use the default endpoint
-    // page URL contains the offset and limit parameters.
-    // essentially the start and finish points of the locations to be fetched.
-    const url = pageURL || `${PokeAPI.baseURL}/location-area`;
-    // Fetch the location object from the PokeAPI
-    const response = await fetch(url);
-    const locations: ShallowLocations = await response.json();
-    // Return the locations object of type ShallowLocations
-    return locations;
-  }
+    closeCache() {
+        this.#cache.stopReapLoop();
+    }
+
+    async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+        // If no pageURL is provided, use the default endpoint
+        // page URL contains the offset and limit parameters.
+        // essentially the start and finish points of the locations to be fetched.
+        const url = pageURL || `${PokeAPI.baseURL}/location-area`;
+        // Check if the URL is already cached and return it instead if it is.
+        const cached = this.#cache.get<ShallowLocations>(url);
+        if (cached) {
+            return cached;
+        }
+        try{
+            // Fetch the location object from the PokeAPI
+            const response = await fetch(url);
+            // Check if the response is ok (status code 200-299)
+            // If not, throw an error with the status code and status text
+            if (!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+
+            const locations: ShallowLocations = await response.json();
+            this.#cache.add(url, locations);
+            
+            // Return the locations object of type ShallowLocations
+            return locations;
+        } catch (err) {
+            throw new Error(`Error fetching locations: ${(err as Error).message}`);
+        }
+    }
 
   // async fetchLocation(locationName: string): Promise<Location> {
     // implement this
